cd9a1c7f1d6abb8865ab6bf6e5ae11cb1aa618a3
Lyubomir Radkov <lyubomirradkov@gmail.com>
2024-10-06 23:37:09 +0300

    Add frontend configuration files and plugins

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        Pantone7604C: "#E4D5D3",
+        Pantone7Black0961C: "#9E978E",
+      },
+    },
+  },
+  plugins: [],
+};
