8faec236eb5a6eff2522ae1e9c6a0d3d3481a875
Lyubomir Radkov <lyubomirradkov@gmail.com>
2025-03-21 22:30:21 +0000

    Update docker-compose for development with nginx

--- .docker/wait_for_postgres.py
+++ .docker/wait_for_postgres.py
@@ -1,9 +1,9 @@
+import logging
 import os
 import time
-import logging
+
 import psycopg2
 from psycopg2 import OperationalError
-
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -19,10 +19,10 @@ def wait_for_postgres():
         try:
             connection = psycopg2.connect(
                 host=os.environ.get("POSTGRES_HOST"),
-                port=os.environ.get('POSTGRES_PORT', default='5432'),
-                user=os.environ.get('POSTGRES_USER'),
-                password=os.environ.get('POSTGRES_PASSWORD'),
-                dbname=os.environ.get('POSTGRES_DB'),
+                port=os.environ.get("POSTGRES_PORT", default="5432"),
+                user=os.environ.get("POSTGRES_USER"),
+                password=os.environ.get("POSTGRES_PASSWORD"),
+                dbname=os.environ.get("POSTGRES_DB"),
                 sslmode=os.environ.get("POSTGRES_SSLMODE"),
                 connect_timeout=TIMEOUT,
             )
@@ -32,7 +32,8 @@ def wait_for_postgres():
         except OperationalError as e:
             if time.time() - start_time > TIMEOUT:
                 logger.error(
-                    f"Timeout reached. Postgres is not available.\n{type(e).__name__}\n{e}")
+                    f"Timeout reached. Postgres is not available.\n{type(e).__name__}\n{e}"
+                )
                 return False
             logger.info("Waiting for Postgres to be available...")
             time.sleep(INTERVAL)
