6e758ef7244134ce7b015d2c83e9abc3cd8f7efb
Lyubomir Radkov <lyubomirradkov@gmail.com>
2025-03-25 10:47:37 +0000

    Refactor blog models, add new features like categories, roles, etc

--- backend/portfolio/blog/models.py
+++ backend/portfolio/blog/models.py
@@ -1,57 +1,185 @@
-from django.db import models
-from django.utils.text import slugify
 import uuid
-from core.utils import remove_exif, create_thumbnail
+
+from blog.forms import BlogPostForm
+from blog.utils import calculate_read_time
+from core.blocks import CarouselBlock, CodeBlock, ExternalLinkBlock
+from django.db import models
+from django.utils.timezone import now
+from modelcluster.fields import ParentalKey
+from modelcluster.tags import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, MultipleChooserPanel
+from wagtail.blocks import RichTextBlock
+from wagtail.fields import StreamField
+from wagtail.models import Orderable, Page
+from wagtail.search import index
+from wagtail_headless_preview.models import HeadlessMixin
 
 
-class Tag(models.Model):
+class BlogPostTag(TaggedItemBase):
+    content_object = ParentalKey(
+        "BlogPostPage", related_name="tagged_items", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = "Blog Post Tag"
+        verbose_name_plural = "Blog Post Tags"
+
+
+class BlogPostCategory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=60)
-
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = "Blog Categories"
 
 
-class BlogPost(models.Model):
+class BlogPostRole(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True, blank=True)
+    name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Blog Role"
+        verbose_name_plural = "Blog Roles"
+
+
+class BlogPostPersonRelationship(Orderable, models.Model):
+    blog_post = ParentalKey(
+        "BlogPostPage",
+        related_name="blog_post_person_relationship",
+        on_delete=models.CASCADE,
+    )
+    person = models.ForeignKey(
+        "core.Person",
+        related_name="person_blog_post_relationship",
+        on_delete=models.CASCADE,
+    )
+    role = models.ForeignKey(
+        BlogPostRole, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    panels = [FieldPanel("person"), FieldPanel("role")]
+
+
+class BlogPostPage(HeadlessMixin, Page):
     excerpt = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
-    tag = models.ManyToManyField(Tag, blank=True)
-    image = models.ImageField(upload_to="blog/images", null=True, blank=True)
-    thumbnail = models.ImageField(upload_to="blog/thumbnails", null=True, blank=True)
-    medium_url = models.URLField(null=True, blank=True)
-    published = models.BooleanField(default=False)
+    content = StreamField(
+        [
+            ("rich_text_block", RichTextBlock(label="Rich Text Block")),
+            ("code_block", CodeBlock()),
+            ("carousel_block", CarouselBlock()),
+        ],
+        null=True,
+        blank=True,
+        use_json_field=True,
+    )
+
+    category = models.ForeignKey(
+        BlogPostCategory, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    tags = ClusterTaggableManager(through=BlogPostTag, blank=True)
+    cover_image = models.ForeignKey(
+        "core.CustomImage",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    cover_image_alt = models.CharField(max_length=255, blank=True)
+    external_link = StreamField(
+        [
+            ("external_link", ExternalLinkBlock()),
+        ],
+        null=True,
+        blank=True,
+        use_json_field=True,
+        max_num=1,
+    )
+    read_time = models.PositiveIntegerField(default=0)
+    date = models.DateField("Post date", default=now)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("slug", help_text="The slug is the URL of the blog post"),
+        FieldPanel("excerpt", help_text="A short description of the blog post"),
+        FieldPanel("content", help_text="Add the content for the blog post"),
+        MultiFieldPanel(
+            [
+                FieldPanel(
+                    "cover_image", help_text="Upload an cover image for the blog post"
+                ),
+                FieldPanel(
+                    "cover_image_alt",
+                    help_text="Add an alternative text for the cover image",
+                ),
+            ],
+            heading="Image",
+        ),
+        FieldPanel("date"),
+        FieldPanel(
+            "external_link", help_text="Link to the blog post on an external site"
+        ),
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        MultipleChooserPanel(
+            "blog_post_person_relationship",
+            chooser_field_name="person",
+            heading="People who contributed to this blog post",
+            label="Person",
+            panels=[
+                FieldPanel("person"),
+                FieldPanel("role", help_text="Role of the person"),
+            ],
+        ),
+        FieldPanel("category", help_text="Select a category for this blog post"),
+        FieldPanel("tags", help_text="Add up to 3 tags for this blog post"),
+    ]
+
+    parent_page_types = ["BlogIndexPage"]
+
+    subpage_types = []
+
+    private_page_options = ["password"]
+
+    base_form_class = BlogPostForm
+
+    search_fields = Page.search_fields + [
+        index.SearchField("title"),
+        index.SearchField("category"),
+        index.RelatedFields(
+            "tagged_items",
+            [
+                index.SearchField("tag"),
+            ],
+        ),
+    ]
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-
-        # Check if the image has been changed or is new
-        if self.image:
-            # Remove EXIF data from the image
-            self.image = remove_exif(self.image)
-            create_thumbnail(self.image, self.thumbnail)
-
+        if self.content:
+            self.read_time = calculate_read_time(self.content)
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['-created_at']
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+        ordering = ["-last_published_at"]
+
+
+class BlogIndexPage(Page):
+    subpage_types = ["BlogPostPage"]
+
+    parent_page_types = ["wagtailcore.Page"]
+
+    max_count = 1
+
+    class Meta:
+        verbose_name = "Blog Index Page"
+        verbose_name_plural = "Blog Index Pages"
