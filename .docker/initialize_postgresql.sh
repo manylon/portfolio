b9ba32da023e96c7c1b048c432e0ead23fd97ba0
Lyubomir Radkov <lyubomirradkov@gmail.com>
2025-05-12 20:03:19 +0000

    Add geoserver database initialization and test data loading

--- .docker/initialize_postgresql.sh
+++ .docker/initialize_postgresql.sh
@@ -5,7 +5,7 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /tmp/initialize_postgresql/init.sql
 
 # List your additional databases here
-DATABASES=("$POSTGRES_DB_BACKEND")
+DATABASES=("$POSTGRES_DB_BACKEND" "$POSTGRES_DB_GEOSERVER")
 
 # Loop through and create each database
 for db in "${DATABASES[@]}"; do
@@ -20,3 +20,13 @@ done
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_BACKEND" -f /tmp/initialize_postgresql/backend.sql
 
 
+# Initialize the geoserver database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_GEOSERVER" -f /tmp/initialize_postgresql/geoserver.sql
+
+# Add test data to the geoserver database
+if [ "$TEST_DATA" = "True" ]; then
+  for sql_file in /tmp/test_data_database/*.sql;
+  do
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_GEOSERVER" -f "$sql_file"
+  done
+fi
